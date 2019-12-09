86bec4d2fe08a2e8a7f49431c98997c490f3395e
Alex <alexcc17@gmail.com>
2019-12-08 21:10:10 -0500

    Initial Commit

---
+++ NewsKit-Demo/MainViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MainViewController.swift
+//  NewsKit-Demo
+//
+//  Created by Alex on 12/8/19.
+//  Copyright © 2019 NewsKit. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+
+    // MARK: - Properties
+    
+    let foodname = ["club sandwich","burger","pasta","pizza","fries","quadracheetosburger","club sandwich","burger","pasta","pizza","fries","quadracheetosburger"]
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - VC Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let edge:CGFloat = 21.0
+        
+        let itemsize = (self.collectionView.frame.width / 2.5)
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.sectionInset = UIEdgeInsets(top: edge, left: edge, bottom: edge, right: edge)
+        
+        layout.itemSize = CGSize(width: itemsize, height: itemsize)
+        
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        self.collectionView.collectionViewLayout = layout
+        
+    }
+    
+
+    // MARK: - Collection View
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("HERE running collectionview")
+        return foodname.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customcell", for: indexPath) as! NewsCollectionViewCell
+        
+        cell.titleLbl.text = foodname[indexPath.row]
+        cell.imageView.image = UIImage(named: "art")
+        return cell
+    }
+}
---
+++ NewsKit-Demo/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  NewsKit-Demo
+//
+//  Created by Alex on 12/8/19.
+//  Copyright © 2019 NewsKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let identifier = "customcell"
+    // MARK: - Outlets
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // MARK: - VC Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize =  CGSize(width: collectionView.bounds.size.width/7 * 3 , height:collectionView.bounds.size.width/7 * 3 )
+        
+        let xib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        collectionView.register(xib, forCellWithReuseIdentifier: identifier)
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // MARK: - Collection View
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 90
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! CollectionViewCell
+        print("HERE incollection view delegate")
+        return cell
+    }
+    
+    
+}
